a020f580491a676f2bf66a74747685287d9e11ab
Ashwin.B <ahnashwin1305@gmail.com>
2020-12-30 22:17:42 +0530

    Update settings.py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ('ab7710850@gmail.com')
-EMAIL_HOST_PASSWORD = ('cr712345')
+EMAIL_HOST_USER = ('')
+EMAIL_HOST_PASSWORD = ('')
